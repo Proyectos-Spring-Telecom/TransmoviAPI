6e2c7c45544591547bb5e22115671bf6afd11ebb
OsmarMartinez <osmar_osmg@hotmail.com>
2025-09-08 18:31:53 -0600

    [fix] usuarios, login

---
+++ src/usuarios/dto/update-usuario-contrasena.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNotEmpty, IsOptional, IsString, Matches, MinLength } from 'class-validator';
+
+export class UpdateUsuarioContrasena {
+  @IsString()
+  @IsNotEmpty({ message: 'El Password es obligatorio' })
+  passwordActual: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El Password es obligatorio' })
+  @MinLength(6, { message: 'El Password debe tener al menos 6 caracteres' })
+  @Matches(/^(?=.*\p{L})(?=.*\d)(?=.*[@$!%*?&.])[^\s]+$/u, {
+    message:
+      'El Password debe contener al menos una letra (UTF-8), un número y un símbolo común (@$!%*?&.)',
+  })
+  @ApiProperty({
+    description: 'Contraseña del usuario',
+    example: 'P@ssword123',
+  })
+  passwordNueva: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El Password es obligatorio' })
+  @MinLength(6, { message: 'El Password debe tener al menos 6 caracteres' })
+  @Matches(/^(?=.*\p{L})(?=.*\d)(?=.*[@$!%*?&.])[^\s]+$/u, {
+    message:
+      'El Password debe contener al menos una letra (UTF-8), un número y un símbolo común (@$!%*?&.)',
+  })
+  @ApiProperty({
+    description: 'Contraseña del usuario',
+    example: 'P@ssword123',
+  })
+  passwordNuevaConfirmacion: string;
+
+}
