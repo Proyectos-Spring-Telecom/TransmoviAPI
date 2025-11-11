99a7e7da2e544eed2467c78d7d5edf0386a6551a
OsmarMartinez <osmar_osmg@hotmail.com>
2025-11-11 14:05:09 -0600

    [fix] bitacora, s3

--- src/bitacora/bitacora.module.ts
+++ src/bitacora/bitacora.module.ts
@@ -3,9 +3,10 @@ import { BitacoraLoggerService } from './bitacora.service';
 import { BitacoraController } from './bitacora.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bitacora } from 'src/entities/Bitacora';
+import { Clientes } from 'src/entities/Clientes';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bitacora])],
+  imports: [TypeOrmModule.forFeature([Bitacora, Clientes])],
   controllers: [BitacoraController],
   providers: [BitacoraLoggerService],
   exports: [BitacoraLoggerService],
--- src/bitacora/bitacora.service.ts
+++ src/bitacora/bitacora.service.ts
@@ -9,21 +9,48 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Bitacora } from 'src/entities/Bitacora';
 import { Repository } from 'typeorm';
 import { ApiResponseCommon } from 'src/common/ApiResponse';
+import { Clientes } from 'src/entities/Clientes';
 
 @Injectable()
 export class BitacoraLoggerService {
   constructor(
     @InjectRepository(Bitacora)
     private readonly bitacoraRepository: Repository<Bitacora>,
+    @InjectRepository(Clientes)
+    private readonly clienteRepository: Repository<Clientes>,
   ) {}
   createBitacora(createBitacoraDto: CreateBitacoraDto) {
     return 'This action adds a new bitacora';
   }
 
-  async findAllListBitacora() {
+  //funcion para obtener los clientes hijos
+  private async clienteHijos(cliente: number) {
+    const clientesFiltrado = await this.clienteRepository.query(
+      `CALL spGetClientes(?);`,
+      [cliente],
+    );
+
+    const idsFiltrados = clientesFiltrado[0]; // El primer índice contiene los resultados
+    const ids = idsFiltrados
+      .map((clientesFiltrado: any) => Number(clientesFiltrado.Id))
+      .filter(Boolean);
+    if (ids.length === 0) {
+      return { data: [] }; // No hay clientes que consultar
+    }
+
+    // 3. Construir el query dinámico con los IDs
+    const placeholders = ids.map(() => '?').join(', ');
+    return { ids, placeholders };
+  }
+
+  async findAllListBitacora(cliente: number, rol: number) {
     try {
-      const bitacora = await this.bitacoraRepository.query(
-        `
+      let bitacora;
+      switch (rol) {
+        case 1:
+          // Consulta de datos listado Usuario SuperAdministrador
+          bitacora = await this.bitacoraRepository.query(
+            `
 SELECT
   -- Bitácora
   b.Id AS id,
@@ -56,7 +83,49 @@ INNER JOIN Modulos m ON b.IdModulo = m.Id
 
 ORDER BY b.FechaCreacion DESC;
             `,
-      );
+          );
+          break;
+
+        default:
+          // Consulta de datos listado resto Usuario
+          const { ids, placeholders } = await this.clienteHijos(cliente);
+          bitacora = await this.bitacoraRepository.query(
+            `
+SELECT
+  -- Bitácora
+  b.Id AS id,
+  b.Modulo AS modulo,
+  b.Descripcion AS descripcion,
+  b.Accion AS accion,
+  b.Query AS query,
+  b.FechaCreacion AS fechaCreacion,
+  b.Estatus AS estatus,
+  b.Error AS error,
+
+  -- Usuario
+  u.Id AS idUsuario,
+  u.Nombre AS nombreUsuario,
+  u.ApellidoPaterno AS apellidoPaternoUsuario,
+  u.ApellidoMaterno AS apellidoMaternoUsuario,
+  u.UserName AS UserNameUsuario,
+  u.Estatus AS estatusUsuario,
+
+  -- Módulo
+  m.Id AS idModulo,
+  m.Nombre AS nombreModulo,
+  m.Descripcion AS descripcionModulo
+
+FROM Bitacora b
+INNER JOIN Usuarios u ON b.IdUsuario = u.Id
+INNER JOIN Modulos m ON b.IdModulo = m.Id
+WHERE u.IdCliente IN (${placeholders})   -- 🔹 aquí colocas el ID del cliente que quieres consultar
+
+ORDER BY b.FechaCreacion DESC;
+            `,
+            [...ids],
+          );
+          break;
+      }
 
       const data = bitacora.map((item) => ({
         ...item,
@@ -73,16 +142,23 @@ ORDER BY b.FechaCreacion DESC;
       if (error instanceof HttpException) {
         throw error;
       }
-      throw new InternalServerErrorException('Ocurrió un error al obtener las bitácoras listado.');
+      throw new InternalServerErrorException(
+        'Ocurrió un error al obtener las bitácoras listado.',
+      );
     }
   }
 
-  async findAll(page: number, limit: number) {
+  async findAll(cliente: number, rol: number, page: number, limit: number) {
     try {
       const offset = (page - 1) * limit;
       let totalResult;
-      const bitacora = await this.bitacoraRepository.query(
-        `
+      let bitacora;
+
+      switch (rol) {
+        case 1:
+          // Consulta de datos paginados Usuario SuperAdministrador
+          bitacora = await this.bitacoraRepository.query(
+            `
 SELECT
   -- Bitácora
   b.Id AS id,
@@ -117,9 +193,9 @@ ORDER BY b.FechaCreacion DESC
 LIMIT ? OFFSET ?;
             `,
             [limit, offset],
-      );
+          );
 
-      // Query para total (sin paginación)
+          // Query para total (sin paginación)
           totalResult = await this.bitacoraRepository.query(
             `
   SELECT COUNT(*) AS total
@@ -128,6 +204,62 @@ INNER JOIN Usuarios u ON b.IdUsuario = u.Id
 INNER JOIN Modulos m ON b.IdModulo = m.Id
   `,
           );
+          break;
+
+        default:
+          // Consulta de datos paginados resto Usuario
+          const { ids, placeholders } = await this.clienteHijos(cliente);
+          bitacora = await this.bitacoraRepository.query(
+            `
+SELECT
+  -- Bitácora
+  b.Id AS id,
+  b.Modulo AS modulo,
+  b.Descripcion AS descripcion,
+  b.Accion AS accion,
+  b.Query AS query,
+  b.FechaCreacion AS fechaCreacion,
+  b.Estatus AS estatus,
+  b.Error AS error,
+
+  -- Usuario
+  u.Id AS idUsuario,
+  u.Nombre AS nombreUsuario,
+  u.ApellidoPaterno AS apellidoPaternoUsuario,
+  u.ApellidoMaterno AS apellidoMaternoUsuario,
+  u.UserName AS UserNameUsuario,
+  u.Estatus AS estatusUsuario,
+
+  -- Módulo
+  m.Id AS idModulo,
+  m.Nombre AS nombreModulo,
+  m.Descripcion AS descripcionModulo
+
+FROM Bitacora b
+INNER JOIN Usuarios u ON b.IdUsuario = u.Id
+INNER JOIN Modulos m ON b.IdModulo = m.Id
+
+WHERE u.IdCliente IN (${placeholders})   -- 🔹 aquí colocas el ID del cliente que quieres consultar
+
+ORDER BY b.FechaCreacion DESC
+LIMIT ? OFFSET ?;
+            `,
+            [...ids, limit, offset],
+          );
+
+          // Query para total (sin paginación)
+          totalResult = await this.bitacoraRepository.query(
+            `
+  SELECT COUNT(*) AS total
+ FROM Bitacora b
+INNER JOIN Usuarios u ON b.IdUsuario = u.Id
+INNER JOIN Modulos m ON b.IdModulo = m.Id
+WHERE u.IdCliente IN (${placeholders})   -- 🔹 aquí colocas el ID del cliente que quieres consultar
+  `,
+  [...ids],
+          );
+          break;
+      }
 
       const total = Number(totalResult[0]?.total ?? 0);
 
@@ -150,7 +282,9 @@ INNER JOIN Modulos m ON b.IdModulo = m.Id
       if (error instanceof HttpException) {
         throw error;
       }
-      throw new InternalServerErrorException('Ocurrió un error al obtener las bitácoras paginada.');
+      throw new InternalServerErrorException(
+        'Ocurrió un error al obtener las bitácoras paginada.',
+      );
     }
   }
 
@@ -190,7 +324,7 @@ WHERE b.Id = ?
 
 ORDER BY b.FechaCreacion DESC;
             `,
-            [id]
+        [id],
       );
 
       if (bitacora.length === 0) {
@@ -203,7 +337,7 @@ ORDER BY b.FechaCreacion DESC;
         idUsuario: Number(item.idUsuario),
         idModulo: Number(item.idModulo),
       }));
-      
+
       return { data: data };
     } catch (error) {
       if (error instanceof HttpException) {
